3574e41c437de3467149837740d8b4a3f319fa33
NetDwarf <NetDwarf@users.noreply.github.com>
2025-02-19 16:58:56 +0100

    Create default config.yml if non exists

--- src/config/yml_parser.rs
+++ src/config/yml_parser.rs
@@ -12,6 +12,7 @@ pub struct ConfigYml {
     #[serde(default)]
     pub exclude: Vec<String>,
     #[serde(default)]
+    #[serde(skip)]
     pub exportignore: Vec<String>,
 }
 
@@ -38,8 +39,28 @@ pub struct SqliteCredentials {
 
 impl ConfigYml {
     pub fn parse() -> ConfigYml {
+        if !fs::exists(CONFIG_FILE_PATH).unwrap() {
+            create_default_yml();
+        }
         let config_yml_text = fs::read_to_string(CONFIG_FILE_PATH).unwrap();
         let config_yml: ConfigYml = serde_yml::from_str(config_yml_text.as_str()).unwrap();
         return config_yml;
     }
+}
+
+fn create_default_yml() {
+    let default_yml = &ConfigYml {
+        db: DbCred::Mysql(MySqlCredentials {
+            host: "localhost".to_string(),
+            user: "dol".to_string(),
+            password: "dol".to_string(),
+            database: "dol".to_string(),
+            port: 3306,
+        }),
+        include: vec![],
+        exclude: vec![],
+        exportignore: vec![],
+    };
+    let contents = serde_yml::to_string(&default_yml).unwrap();
+    fs::write(CONFIG_FILE_PATH, contents.as_bytes()).unwrap();
 }
